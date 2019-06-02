c2df6bc1df8b034329cdd847baf185df45eb96aa
minhphan2603 <48484833+minhphan2603@users.noreply.github.com>
2019-06-03 00:19:51 +0700

    Updates

---
+++ precache-manifest.80dd7a9c779c1b0de1513a57b083aaa0.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2dc8432a75270da8d4f29aa2ecf88cfa",
+    "url": "/DressingRoom/index.html"
+  },
+  {
+    "revision": "a5d5a5b91431e4fdbb7f",
+    "url": "/DressingRoom/static/css/main.f3ac6aa2.chunk.css"
+  },
+  {
+    "revision": "219635517f5a5e3c9873",
+    "url": "/DressingRoom/static/js/2.bc5fbade.chunk.js"
+  },
+  {
+    "revision": "a5d5a5b91431e4fdbb7f",
+    "url": "/DressingRoom/static/js/main.7262ee0a.chunk.js"
+  },
+  {
+    "revision": "712479577c1719d77426",
+    "url": "/DressingRoom/static/js/runtime~main.63c51b7e.js"
+  }
+]);
